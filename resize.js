e7886145a41f76127519a9a716be5ab2be042b87
Brent Brownfield <bbrownfield@gmail.com>
2020-11-12 08:58:47 -0500

    Add the resize functionality

--- resize.js
+++ resize.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
-try {
 const sharp = require('sharp');
+
+// Exceptions seem to be swallowed in termux
+// If running on termux, place the sharp require in the
+// following block and comment out the contents of
+// the resize function below
+try {
 } catch(e) {
     console.log("Exception in require: " + e.message);
 }
 
 module.exports = function resize(path, format, width, height) {
     const readStream = fs.createReadStream(path);
-    return readStream;
+    let transform = sharp();
+
+    if(format) {
+        transform = transform.toFormat(format);
+    }
+
+    if(width || height) {
+        transform = transform.resize(width, height);
+    }
+
+    return readStream.pipe(transform);
 }
